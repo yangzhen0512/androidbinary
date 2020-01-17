d273851af4428afb16f9ddbbb3274bcb4c833237
yangzhen0512 <619278982@qq.com>
2020-01-17 15:16:10 +0800

    fix import

--- example_test.go
+++ example_test.go
@@ -5,8 +5,8 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/shogo82148/androidbinary"
-	"github.com/shogo82148/androidbinary/apk"
+	"github.com/yangzhen0512/androidbinary"
+	"github.com/yangzhen0512/androidbinary/apk"
 )
 
 func ExampleNewXMLFile() {
